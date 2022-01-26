716ca5d1c6dc8897abf52b3652f696031a76c706
boriskleshch <b.kleshch@bsg.world>
2022-01-27 00:14:58 +0200

    updated 2

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,17 @@
 $( document ).ready(function() {
-
+    var mql = window.matchMedia('(min-width: 1368px)').matches;
     $(window).on('scroll', function () {
-        var mql = window.matchMedia('(min-width: 1024px)').matches;
         if (mql){
             var st = $(this).scrollTop();
             document.querySelector(".for__bg").style.transform = `scale(${1 + st*0.0002})`;
+        }else {
+            document.querySelector(".for__bg").style.transform = `scale(1)`;
         }
-    })
-
+    });
+    if (!mql){
+        $('.hamburger').click(function(){
+            $(this).toggleClass('is-active');
+            $('.navigation__collector').toggleClass('active');
+        });
+    }
 });
