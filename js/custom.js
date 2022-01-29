e87ec80072d49f533765777038e880044502eccd
boriskleshch <b.kleshch@bsg.world>
2022-01-29 17:07:21 +0200

    effects upd

--- js/custom.js
+++ js/custom.js
@@ -19,7 +19,6 @@ $( document ).ready(function() {
     }
 
     $('.functionality-item__title').click(function () {
-        $('.functionality__items').addClass('functionality__items_active');
         $('.functionality-item__title').parent().removeClass('functionality-item_active');
         $(this).parent().addClass('functionality-item_active');
     });
